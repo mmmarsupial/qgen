b30969711c64f593b1dd30aa67d7ca8dcd9a5289
mathias <mathias.stenlund@telia.com>
2024-01-04 17:11:39 +0100

    Changed name to "evaluate.py" from "evaluate_model.py". Metric class and Prepare data class now takes a tokenizer as an arguments instead of model name.

---
+++ secondary/metric.py
@@ -0,0 +1,38 @@
+from transformers import AutoTokenizer
+from datasets import load_metric
+import numpy as np
+
+
+
+class CalculateMetric:
+    def __init__(self, tokenizer, metric="rouge"):
+        self.tokenizer = tokenizer
+        self.metric = load_metric(metric)
+
+    # Computes the metric for the predicted output given the gold output
+    def compute_metrics(self, pred_labs):
+        
+        tokenized_preds, tokenized_labs = pred_labs
+        preds, labs = self._detokenize(tokenized_preds, tokenized_labs)
+        scores = self.metric.compute(predictions=preds, 
+                                     references=labs,
+                                     use_stemmer=True)
+        return {key: round(value.mid.fmeasure * 100, 2) for key, value in scores.items()}
+        
+    # Detokenizers using the given model tokenizer
+    def _detokenize(self, tokenized_preds, tokenized_labs):
+
+        preds = self.tokenizer.batch_decode(tokenized_preds, skip_special_tokens=True)
+        tokenized_labs = np.where(tokenized_labs != -100, 
+                                  tokenized_labs, 
+                                  self.tokenizer.pad_token_id)
+        labs = self.tokenizer.batch_decode(tokenized_labs, skip_special_tokens=True)
+        return preds, labs
+    
+    # To compute scores for already detokenized output
+    def compute_metrics_detokenized(self, pred_labs):
+        preds, labs = pred_labs
+        scores = self.metric.compute(predictions=preds, 
+                                     references=labs,
+                                     use_stemmer=True)
+        return {key: round(value.mid.fmeasure * 100, 2) for key, value in scores.items()}
--- test.py
+++ test.py
@@ -1,22 +1,39 @@
 import pandas as pd
 from transformers import AutoTokenizer
-from prepare_data import DataPrep
-from metric import CalculateMetric
+from secondary.prepare_data import DataPrep
+from secondary.metric import CalculateMetric
+from sys import argv, exit
+from getopt import getopt
+
+def main(argv):
+    print()
+    print("Fine-tune a model for question generation given a claim")
+    print()
+
+    model_name = "t5-small"                                     # default
+    output_dir = "./checkpoints/" + model_name + "-fine-tuned"  # default
+    use_full_data = True                                        # default
 
 
-model_name = "t5-small"
-max_length_in = 200
-max_length_out = 100
+    opts, _ = getopt(argv, "hi:o:f")
+    for opt, arg in opts:
+        if opt == "-h":
+            print("Usage: train.py [-h] [-i <base pre-trained model>] [-o <output path>]")
+            print ("-h\t\tPrints usage information.")
+            print ("-i <model>\tSets pre-trained model. Default is t5-small.")
+            print ("-o <model>\tSets the output path for the fine-tuned model")
+            print ()
+            exit()
+        elif opt == "-i":
+            model_name = arg
+        elif opt == "-o":
+            output_dir = arg
+        elif opt == "-f":
+            use_full_data = False
+    
+    print("Pre-trained model: ", model_name)
+    print("Output path is: ", output_dir)
+    print("Uses full data: ", use_full_data)
 
-#tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-#dp = DataPrep(model_name)
-
-#test_data = dp.prepare_data("./claim_datasets/test_short.csv")
-
-#df = pd.DataFrame({
-#    'name': ['Alice', 'Bob', 'Charlie'],
-#    'age': [25, 30, 35]
-#})
-
-metric = CalculateMetric(model_name, metric="rouge")
+if __name__ == "__main__":
+    main(argv[1:])
